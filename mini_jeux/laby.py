53872f255b533956a968e0e5f7eed960a1605b63
rapha <raphaelprost71@gmail.com>
2025-02-11 15:34:51 +0100

    car et fire au bon endroit

--- mini_jeux/laby.py
+++ mini_jeux/laby.py
@@ -1,10 +1,9 @@
 from random import randint
-
 from carte import Carte
 import pygame
 import settings
 from enemy import Enemy
-import random
+
 class Laby(Carte):
     """
     Ce n'est pas un mini jeux juste un test de truc random
@@ -13,36 +12,35 @@ class Laby(Carte):
     def __init__(self):
         super().__init__("map/map.tmx")
         self.radius = 100
-        self.orageframe = 0
+        self.timer = 0
+        self.orageframe = self.timer
         self.backcolor = (0, 0, 0)
-        self.timetorche = 100
+        self.roundcolor = (0, 0, 0, 0)
         self.enemytexture = Enemy("img/fire.png",30,30)
         self.groupe.add(self.enemytexture)
         self.zoom = settings.ZOOM
     def __str__(self):
         return "Laby"
     def eventenemy(self):
-        self.timetorche = settings.FPS*5 #tps * sec
+        self.radius = settings.FPS*5 #tps * sec
     def add_draw(self, screen):
         mask = pygame.Surface((800, 600), pygame.SRCALPHA)
         coord = self.fixe_coord(self.player.rect.center)
         mask.fill(self.backcolor)
-        pygame.draw.circle(mask, (0, 0, 0, 0), (coord[0], coord[1]), self.radius)
+        pygame.draw.circle(mask, self.roundcolor, (coord[0], coord[1]), self.radius)
         screen.blit(mask, (0, 0))
     def add_verif(self):
-        if self.timetorche !=100:
-            self.timetorche -= 1
-        if self.orageframe != 0:
-            self.orageframe -= 1
-        if self.orageframe == 0:
+        if self.radius !=100:
+            self.radius -= 1
+        if self.orageframe <= self.timer:
             self.backcolor = (0,0,0,255)
-            self.radius = self.timetorche
-        if self.timetorche == 100:
-            self.radius = 100
+            self.roundcolor = (0,0,0,0)
         if self.collision(self.enemytexture.rect):
             self.eventenemy()
+        self.timer += 1
         self.orage()
     def orage(self):
         if randint(0,600) == 1 and self.orageframe == 0:
             self.backcolor = (255,255,0,120)
-            self.orageframe = 15
+            self.roundcolor = (255, 255, 0, 120)
+            self.orageframe = self.timer + 15
