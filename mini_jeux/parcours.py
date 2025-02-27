f9b93da0feea5375d6504b8b30728b46d6f04b9c
Nukex64 <ibanezanthony99@gmail.com>
2025-02-27 18:07:59 +0100

    Ajout : meilleur systeme de jump

--- mini_jeux/parcours.py
+++ mini_jeux/parcours.py
@@ -1,5 +1,7 @@
 #Exemple
 from carte import Carte
+
+
 class Game_Jump(Carte):
     """
     Exemple de mini jeux de jump / parcoure
@@ -9,17 +11,19 @@ class Game_Jump(Carte):
         self.liste_echelle = self.objets_par_classe('echelle') # on recupere les echelles
         self.sortie = self.objets_par_classe('sortie')
         self.liste_sol = self.objet_par_calque('sol') # on recupere les sols
-        self.time_jump = 0 # timer de jump
+
+        self.frame_jump = 35 # timer de jump
+        self.last_y = 0
+        self.start_y = 0
 
     def add_verif(self):
-        if self.time_jump: # jump
-            self.player.haut(1.75) # effet 1.75 - gravité = 0.75 (voir player)
-            self.time_jump -= 1
+        if self.frame_jump > 0:
+            self.jump()
 
         if self.multi_collision(self.liste_echelle):
-            self.player.haut() # contre l'effet de la gravité sur les echelles
+            self.anti_gravity()
 
-        self.player.bas() # gravité
+        self.gravity()
 
     def clavier(self):
         if self.touche("d"):
@@ -37,9 +41,11 @@ class Game_Jump(Carte):
             if self.touche("s"):
                 self.player.bas()
 
-        if self.multi_collision(self.liste_sol):
+        if self.multi_collision(self.liste_sol) and self.frame_jump == 0:
             if self.touche("SPACE"): # jump
-                self.time_jump = 200*0.06 #200ms (0.06 car 60/s = 0.06/ms)
+                self.frame_jump = 36 #36
+                self.start_y = self.player.coord[1]
+                self.last_y  = self.player.coord[1]
 
 
 
@@ -50,4 +56,24 @@ class Game_Jump(Carte):
 
     def __str__(self):
         return "Parcours"
+
+    @staticmethod
+    def f(x):
+        return (x * (36 - x)) / 162
+
+    def gravity(self):
+        self.player.vy += 2
+
+    def anti_gravity(self):
+        self.player.vy -= 2
+
+    def jump(self):
+        self.anti_gravity()
+        new_y = self.f(self.frame_jump)*16 + self.start_y #nouvelle coordonné
+        vy = new_y - self.last_y # de combien il doit monter
+        print(new_y, self.frame_jump)
+        self.player.vy -= vy
+        self.last_y = new_y
+        self.frame_jump -= 1
+
 
