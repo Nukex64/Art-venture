41957ceee630c39654ce8be69cf9c485ea8e0900
skioushi <matheofaure71@gmail.com>
2025-03-19 17:20:59 +0100

    Avancées sur bubble game

--- mini_jeux/swim.py
+++ mini_jeux/swim.py
@@ -1,6 +1,7 @@
 import pygame
 from carte import Carte
 from enemy import Enemy
+from random import randint
 
 class Swim(Carte):
     def __init__(self):
@@ -8,21 +9,25 @@ class Swim(Carte):
         self.temps = 5 * 60
         self.decompte = self.temps
         self.bubbles = []
-        self.spawn_bubbles = [(0, 0), (10, 10), (40, 70), (40, 20)]
+        self.spawn_bubbles = [(randint(5, 380), randint(5, 300)), (randint(5, 380), randint(5, 300)), (randint(5, 380), randint(5, 300)),(randint(5, 380), randint(5, 300))]
+        self.player.speed = 1
         self.spawn()
-
     def add_verif(self):
         if self.decompte <= 0:
             exit()
-        for bubble in self.bubbles:
-            if self.collision(bubble):
-                self.groupe.remove(bubble)
-                self.bubbles.remove(bubble)
+        for bub in self.bubbles:
+            if self.sprite_collision(bub):
+                self.decompte = self.temps
+                self.groupe.remove(bub)
+                self.bubbles.remove(bub)
+                print(self.decompte)
+
         self.decompte -= 1
 
     def spawn(self):
-        for x in self.spawn_bubbles:
-            bubble = Enemy("img/bubble.png", 40, 40)
+        for x in range(len(self.spawn_bubbles)):
+            bubble = Enemy("img/bubble.png", self.spawn_bubbles[x][0]  , self.spawn_bubbles[x][1])
+            self.bubbles.append(bubble)
             self.groupe.add(bubble)
             self.decompte = self.temps
 
