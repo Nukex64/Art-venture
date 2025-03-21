c06a02819a657e3b24f89bb97b53ec0036acef96
skioushi <matheofaure71@gmail.com>
2025-03-21 16:48:59 +0100

    grosses avancées sur bubble game, anthony check stp

--- mini_jeux/swim.py
+++ mini_jeux/swim.py
@@ -9,9 +9,13 @@ class Swim(Carte):
         self.temps = 5 * 60
         self.decompte = self.temps
         self.bubbles = []
-        self.spawn_bubbles = [(randint(5, 380), randint(5, 300)), (randint(5, 380), randint(5, 300)), (randint(5, 380), randint(5, 300)),(randint(5, 380), randint(5, 300))]
-        self.player.speed = 1
+        self.spawn_bubbles = [(16.8333, 180.5), (155.63, 30.7), (225.23, 73.3), (138.83, 201.7), (80.03, 167.5), (19.433, 319.9), (167.033, 370.9), (336.23, 376.9), (230.83, 372.5), (219.23, 248.5), (198.233, 188.5), (360.8333, 125.5)]
+        self.coin = Enemy("img/coin.png", 372.8333, 368.5)
+        self.groupe.add(self.coin)
+        self.has_key = 0
+        self.player.speed = 0.6
         self.spawn()
+
     def add_verif(self):
         if self.decompte <= 0:
             exit()
@@ -21,8 +25,11 @@ class Swim(Carte):
                 self.groupe.remove(bub)
                 self.bubbles.remove(bub)
                 print(self.decompte)
-
+        print(self.player.coord)
         self.decompte -= 1
+        if self.sprite_collision(self.coin):
+            self.groupe.remove(self.coin)
+            self.has_key = 1
 
     def spawn(self):
         for x in range(len(self.spawn_bubbles)):
@@ -30,9 +37,8 @@ class Swim(Carte):
             self.bubbles.append(bubble)
             self.groupe.add(bubble)
             self.decompte = self.temps
-
     def add_draw(self, screen):
-        texte_render = self.font.render(str(round(self.decompte/60, 2)), True, (0, 0, 0))
+        texte_render = self.font.render(str(round(self.decompte/60, 2)), True, (255, 255, 255))
         screen.blit(texte_render, (0, 0))
 
 
