5fd6707db6d2a6787bdad9b25fca0e3d7f51f184
Nukex64 <ibanezanthony99@gmail.com>
2025-03-23 20:43:00 +0100

    Ajout map

---
+++ map/tileset_tsx/Musée.tsx
@@ -0,0 +1,158 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="Musée" tilewidth="64" tileheight="64" tilecount="50" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="1">
+  <image source="../tileset_img/my_tile/mur_gris_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="2">
+  <image source="../tileset_img/my_tile/1739285363370.png" width="16" height="16"/>
+ </tile>
+ <tile id="4">
+  <image source="../tileset_img/my_tile/bordure_tab_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="5">
+  <image source="../tileset_img/my_tile/Museum Wall (bas).png" width="16" height="16"/>
+ </tile>
+ <tile id="6">
+  <image source="../tileset_img/my_tile/chandelier.png" width="16" height="16"/>
+ </tile>
+ <tile id="7">
+  <image source="../tileset_img/my_tile/chev_b.png" width="16" height="16"/>
+ </tile>
+ <tile id="8">
+  <image source="../tileset_img/my_tile/chev_h.png" width="16" height="16"/>
+ </tile>
+ <tile id="9">
+  <image source="../tileset_img/my_tile/ex_tab.png" width="16" height="16"/>
+ </tile>
+ <tile id="10">
+  <image source="../tileset_img/my_tile/herbe2.png" width="16" height="16"/>
+ </tile>
+ <tile id="11">
+  <image source="../tileset_img/my_tile/mur2.png" width="16" height="16"/>
+ </tile>
+ <tile id="12">
+  <image source="../tileset_img/my_tile/murgris.png" width="16" height="16"/>
+ </tile>
+ <tile id="13">
+  <image source="../tileset_img/my_tile/murgris2.png" width="16" height="16"/>
+ </tile>
+ <tile id="14">
+  <image source="../tileset_img/my_tile/Museum Floor Cross Square (1).png" width="16" height="16"/>
+ </tile>
+ <tile id="15">
+  <image source="../tileset_img/my_tile/porte_bas.png" width="16" height="16"/>
+ </tile>
+ <tile id="16">
+  <image source="../tileset_img/my_tile/porte_haut_pd.png" width="16" height="16"/>
+ </tile>
+ <tile id="17">
+  <image source="../tileset_img/my_tile/pp_bg.png" width="16" height="16"/>
+ </tile>
+ <tile id="18">
+  <image source="../tileset_img/my_tile/pp_hd.png" width="16" height="16"/>
+ </tile>
+ <tile id="19">
+  <image source="../tileset_img/my_tile/pp_hg.png" width="16" height="16"/>
+ </tile>
+ <tile id="20">
+  <image source="../tileset_img/my_tile/Stones Wall.png" width="16" height="16"/>
+ </tile>
+ <tile id="21">
+  <image source="../tileset_img/my_tile/tapis2.png" width="16" height="16"/>
+ </tile>
+ <tile id="22">
+  <image source="../tileset_img/my_tile/bordure_tab_3.png" width="16" height="16"/>
+ </tile>
+ <tile id="23">
+  <image source="../tileset_img/my_tile/herbe1.png" width="16" height="16"/>
+ </tile>
+ <tile id="24">
+  <image source="../tileset_img/my_tile/murrouge2.png" width="16" height="16"/>
+ </tile>
+ <tile id="25">
+  <image source="../tileset_img/my_tile/murrouge3.png" width="16" height="16"/>
+ </tile>
+ <tile id="26">
+  <image source="../tileset_img/my_tile/Pilier base.png" width="16" height="16"/>
+ </tile>
+ <tile id="27">
+  <image source="../tileset_img/my_tile/Pilier haut.png" width="16" height="16"/>
+ </tile>
+ <tile id="28">
+  <image source="../tileset_img/my_tile/Pilier milieu.png" width="16" height="16"/>
+ </tile>
+ <tile id="29">
+  <image source="../tileset_img/my_tile/porte_haut_pg.png" width="16" height="16"/>
+ </tile>
+ <tile id="30">
+  <image source="../tileset_img/my_tile/SOL 2.png" width="16" height="16"/>
+ </tile>
+ <tile id="31">
+  <image source="../tileset_img/my_tile/TAPIS.png" width="16" height="16"/>
+ </tile>
+ <tile id="32">
+  <image source="../tileset_img/my_tile/Mur_bas_.png" width="16" height="16"/>
+ </tile>
+ <tile id="33">
+  <image source="../tileset_img/my_tile/Museum Floor Cross Square.png" width="16" height="16"/>
+ </tile>
+ <tile id="34">
+  <image source="../tileset_img/my_tile/herbe.png" width="16" height="16"/>
+ </tile>
+ <tile id="35">
+  <image source="../tileset_img/my_tile/fontaine3.png" width="64" height="64"/>
+  <animation>
+   <frame tileid="35" duration="1000"/>
+   <frame tileid="36" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="36">
+  <image source="../tileset_img/my_tile/fontaine1.png" width="64" height="64"/>
+ </tile>
+ <tile id="38">
+  <image source="../tileset_img/my_tile/bord_mur_angle.png" width="16" height="16"/>
+ </tile>
+ <tile id="39">
+  <image source="../tileset_img/my_tile/murgris5.png" width="16" height="16"/>
+ </tile>
+ <tile id="40">
+  <image source="../tileset_img/my_tile/murgris4.png" width="16" height="16"/>
+ </tile>
+ <tile id="41">
+  <image source="../tileset_img/my_tile/murgris3.png" width="16" height="16"/>
+ </tile>
+ <tile id="42">
+  <image source="../tileset_img/my_tile/murgris8.png" width="16" height="16"/>
+ </tile>
+ <tile id="43">
+  <image source="../tileset_img/my_tile/murgris7.png" width="16" height="16"/>
+ </tile>
+ <tile id="44">
+  <image source="../tileset_img/my_tile/murgris6.png" width="16" height="16"/>
+ </tile>
+ <tile id="45">
+  <image source="../tileset_img/my_tile/murgris9.png" width="16" height="16"/>
+ </tile>
+ <tile id="46">
+  <image source="../tileset_img/my_tile/murgris10.png" width="16" height="16"/>
+ </tile>
+ <tile id="47">
+  <image source="../tileset_img/my_tile/bord_mur.png" width="16" height="16"/>
+ </tile>
+ <tile id="48">
+  <image source="../tileset_img/my_tile/bord_mur_3.png" width="16" height="16"/>
+ </tile>
+ <tile id="49">
+  <image source="../tileset_img/my_tile/bord_mur_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="54">
+  <image source="../tileset_img/my_tile/coin_2.png" width="16" height="16"/>
+ </tile>
+ <tile id="55">
+  <image source="../tileset_img/my_tile/coin_1.png" width="16" height="16"/>
+ </tile>
+ <tile id="56">
+  <image source="../tileset_img/my_tile/coin.png" width="16" height="16"/>
+ </tile>
+</tileset>
