f4dc1ca61ecf0c8c1e3e628471578f35a1f8179c
Nukex64 <ibanezanthony99@gmail.com>
2025-03-25 19:45:19 +0100

    Fin quiz

--- mini_jeux/quiz.py
+++ mini_jeux/quiz.py
@@ -9,19 +9,21 @@ class Quiz:
     def __init__(self):
         with open(f"tableau.json", "r+") as f:
             self.data = json.load(f)
-            f.close()
 
+        self.timer = 0
         self.objetif = None
         self.nb = randint(1, 10)
         self.q_nb = randint(1, 3)
         self.q = {2: "Qui est l'auteur?", 3:"Quelle est la date ?", 1:"Quelle est le titre ?"}
 
         self.bonne_reponse = [self.data[str(self.nb)][self.q_nb]]
-        mauvaise_r_nb = []
-        while len(mauvaise_r_nb) < 4:
+
+        self.mauvaise_r = []
+        while len(self.mauvaise_r) < 3:
             x = randint(1, 10)
-            if x not in mauvaise_r_nb : mauvaise_r_nb.append(x)
-        self.mauvaise_r = [self.data[str(nb)][self.q_nb] for nb in mauvaise_r_nb]
+            txt = self.data[str(x)][self.q_nb]
+            if txt not in self.mauvaise_r : self.mauvaise_r.append(txt)
+
         self.reponses = self.bonne_reponse + self.mauvaise_r
 
         self.img = pygame.image.load(f"img/tableau/{self.nb}.webp")
@@ -38,10 +40,10 @@ class Quiz:
         self.r_4 = self.font_2.render(self.reponses[3], False, (0, 0, 0))
 
         self.rect_1 = pygame.Rect(100, 415, self.r_1.get_width(), 19)
-        self.rect_2 = pygame.Rect(400, 415, self.r_1.get_width(), 19)
-        self.rect_3 = pygame.Rect(100, 500, self.r_1.get_width(), 19)
-        self.rect_4 = pygame.Rect(400, 500, self.r_1.get_width(), 19)
-        self.rects = {1:self.rect_1, 2:self.rect_2, 3:self.rect_3, 4:self.rect_4}
+        self.rect_2 = pygame.Rect(400, 415, self.r_2.get_width(), 19)
+        self.rect_3 = pygame.Rect(100, 500, self.r_3.get_width(), 19)
+        self.rect_4 = pygame.Rect(400, 500, self.r_4.get_width(), 19)
+        self.rects = {0:self.rect_1, 1:self.rect_2, 2:self.rect_3, 3:self.rect_4}
         self.rect_afficher = [(0, 0, 0, 0), (0, 0, 0, 0)]
 
     def draw(self, screen):
@@ -57,12 +59,11 @@ class Quiz:
         screen.blit(self.img, (x, 10))
 
         if self.rect_afficher != [(0, 0, 0, 0), (0, 0, 0, 0)]:
-            print("ok")
-            pygame.draw.rect(screen, (255, 0, 0), self.rect_afficher[0], 2)
+            pygame.draw.rect(screen, (0, 255, 0), self.rect_afficher[0], 2)
             pygame.draw.rect(screen, (255, 0, 0), self.rect_afficher[1], 2)
-            pygame.display.flip()
-            pygame.time.wait(1000)
-            #self.change()
+            self.timer -= 1
+            if self.timer <= 0: self.change()
+
 
     def keypressed(self, event):
         if event.key == pygame.K_z:
@@ -74,11 +75,11 @@ class Quiz:
         self.q = {2: "Qui est l'auteur?", 3:"Quelle est la date ?", 1:"Quelle est le titre ?"}
 
         self.bonne_reponse = [self.data[str(self.nb)][self.q_nb]]
-        mauvaise_r_nb = []
-        while len(mauvaise_r_nb) < 4:
+        self.mauvaise_r = []
+        while len(self.mauvaise_r) < 3:
             x = randint(1, 10)
-            if x not in mauvaise_r_nb : mauvaise_r_nb.append(x)
-        self.mauvaise_r = [self.data[str(nb)][self.q_nb] for nb in mauvaise_r_nb]
+            txt = self.data[str(x)][self.q_nb]
+            if txt not in self.mauvaise_r : self.mauvaise_r.append(txt)
         self.reponses = self.bonne_reponse + self.mauvaise_r
 
         self.img = pygame.image.load(f"img/tableau/{self.nb}.webp")
@@ -89,32 +90,34 @@ class Quiz:
 
         shuffle(self.reponses)
         self.good =self.reponses.index(self.bonne_reponse[0])
+
         self.r_1 = self.font_2.render(self.reponses[0], False, (0, 0, 0))
         self.r_2 = self.font_2.render(self.reponses[1], False, (0, 0, 0))
         self.r_3 = self.font_2.render(self.reponses[2], False, (0, 0, 0))
         self.r_4 = self.font_2.render(self.reponses[3], False, (0, 0, 0))
 
         self.rect_1 = pygame.Rect(100, 415, self.r_1.get_width(), 19)
-        self.rect_2 = pygame.Rect(400, 415, self.r_1.get_width(), 19)
-        self.rect_3 = pygame.Rect(100, 500, self.r_1.get_width(), 19)
-        self.rect_4 = pygame.Rect(400, 500, self.r_1.get_width(), 19)
-        self.rects = {1: self.rect_1, 2: self.rect_2, 3: self.rect_3, 4: self.rect_4}
+        self.rect_2 = pygame.Rect(400, 415, self.r_2.get_width(), 19)
+        self.rect_3 = pygame.Rect(100, 500, self.r_3.get_width(), 19)
+        self.rect_4 = pygame.Rect(400, 500, self.r_4.get_width(), 19)
+        self.rects = {0: self.rect_1, 1: self.rect_2, 2: self.rect_3, 3: self.rect_4}
         self.rect_afficher = [(0, 0, 0, 0), (0, 0, 0, 0)]
-
+        print(self.good)
     def __str__(self):
         return "Quiz"
 
     def choice(self, nb):
-        if nb == self.good:
-            self.rect_afficher = [self.rects[self.good], self.rects[nb]]
-            print("OUI")
-        else:
-            self.rect_afficher = [(0, 0, 0, 0), self.rects[nb]]
+        if self.timer <= 0:
+            self.timer = 100
+            if nb == self.good:
+                self.rect_afficher = [self.rects[self.good], (0, 0, 0, 0)]
+            else:
+                self.rect_afficher = [self.rects[self.good], self.rects[nb]]
 
     def update(self):
-        if pygame.mouse.get_pressed()[0]:
+        if pygame.mouse.get_pressed()[0] :
             x, y = pygame.mouse.get_pos()
-            if self.rect_1.collidepoint(x, y): self.choice(1)
-            if self.rect_2.collidepoint(x, y): self.choice(2)
-            if self.rect_3.collidepoint(x, y): self.choice(3)
-            if self.rect_4.collidepoint(x, y): self.choice(4)
+            if self.rect_1.collidepoint(x, y): self.choice(0)
+            if self.rect_2.collidepoint(x, y): self.choice(1)
+            if self.rect_3.collidepoint(x, y): self.choice(2)
+            if self.rect_4.collidepoint(x, y): self.choice(3)
